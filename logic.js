5740045b045d448f95500b61dcdaf73e5ec05c09
abdou <113465542+abdou-naab@users.noreply.github.com>
2023-08-29 16:29:42 +0100

    form logic + adding books elms left

---
+++ logic.js
@@ -0,0 +1,23 @@
+const addBtn = document.querySelector(".btn-add");
+const modal = document.querySelector("form");
+const firstField = document.querySelector("#title");
+const container = document.querySelector(".container");
+// const clickElse = document.querySelectorAll("body :not(form, .btn-add)");
+
+// addBtn.addEventListener("click", () => {
+//   modal.style.display = "grid";
+//   container.classList.toggle("modal-open");
+//   firstField.focus();
+//   if (container.classList.contains("modal-open")) {
+//     addBtn.disabled = true;
+//   } else {
+//     addBtn.disabled = false;
+//   }
+// });
+
+// clickElse.forEach((e)=>{
+
+// })
+// document.addEventListener("click", () => {
+//   modal.style.display = "none";
+// });
